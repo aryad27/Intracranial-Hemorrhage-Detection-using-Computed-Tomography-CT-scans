ea11a8d33c6d183831624555614de09c0f5a56aa
Arya Dwivedi <53875131+aryad27@users.noreply.github.com>
2022-02-25 13:02:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Intracranial-Hemorrhage-Detection-using-Computed-Tomography-CT-scans.
-Developed a light-weight deep learning model to detect Intracranial Hemorrhage(ICH) using Computed Tomography(CT) scans. 
+# ReSGAN-ICH
+
+This repository contains code for learning semantic manipulation of head CT-scans as described in the paper ReSGAN: Intracranial Hemorrhage Segmentation with Residuals of Synthetic Brain CT Scans. 
+
+The current version is quickly adapted by removing private components from project. Requirements and running instructions will be added later.
+
+## Credit
+
+Inspiration is taken and some parts of the code are borrowed from repositories below.
+- https://github.com/NVlabs/SPADE
+- https://github.com/tzt101/CLADE
+- https://github.com/taesungp/contrastive-unpaired-translation.
---
+++ create_dataloader.py
@@ -0,0 +1,22 @@
+# We can use an image folder dataset the way we have it setup.
+# Create the dataset
+dataset = dset.ImageFolder(root=dataroot,
+                           transform=transforms.Compose([
+                               transforms.Resize(image_size),
+                               transforms.CenterCrop(image_size),
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                           ]))
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                         shuffle=True, num_workers=workers)
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+# Plot some training images
+real_batch = next(iter(dataloader))
+plt.figure(figsize=(8,8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
